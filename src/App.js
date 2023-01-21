75313618cbdfb0c1bc2cc14f6c2858b5e788ce7b
Danny <barakadan421@gmail.com>
2023-01-21 18:53:05 +0300

    Wrap App in Provider and access the store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Greetings from './components/Greetings';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        < Greetings />
+      <Provider store={store}>
+        <Greetings/>
+      </Provider>,
       </header>
     </div>
   );
