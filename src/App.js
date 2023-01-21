36d51af64bba6ae67727db1a8bea15b63771fe82
Danny <barakadan421@gmail.com>
2023-01-21 18:46:49 +0300

    Greetings component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Greetings from './components/Greetings';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Starting point</h1>
+        < Greetings />
       </header>
     </div>
   );
---
+++ src/components/Greetings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greetings() {
+  return (
+    <div>
+      <h1>Greetings from component</h1>
+    </div>
+  )
+}
+
+export default Greetings
