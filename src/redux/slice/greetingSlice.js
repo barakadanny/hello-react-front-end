96749fa220fa6fc3d3ecf7979ec654e44d75b073
Danny <barakadan421@gmail.com>
2023-02-04 12:30:07 +0300

    Api end point Added

--- src/redux/slice/greetingSlice.js
+++ src/redux/slice/greetingSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const fetchGreetings = createAsyncThunk(
     "greeting/fetchGreetings",
     async () => {
-        const response = await fetch("/api/greeting");
+        const response = await fetch("http://127.0.0.1:3000/api/v1/greetings/random");
         const data = await response.json();
         return data;
     }
